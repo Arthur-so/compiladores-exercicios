236a9c06123c53cc07cc0535be4232593568cdb1
arthur <arthursantos.oliveira001@gmail.com>
2024-11-06 17:40:34 -0300

    classificação das palavras chaves

--- ex3/funcs.c
+++ ex3/funcs.c
@@ -5,6 +5,18 @@
 
 void fill_buffer(FILE *fptr, Buffer *buffer);
 
+KeyWord key_words[] = {
+    {"int", INT}, {"char", CHAR}, {"float", FLOAT}, {"double", DOUBLE},
+    {"long", LONG}, {"auto", AUTO}, {"break", BREAK}, {"case", CASE},
+    {"continue", CONTINUE}, {"default", DEFAULT}, {"do", DO},
+    {"enum", ENUM}, {"extern", EXTERN}, {"for", FOR}, {"goto", GOTO},
+    {"if", IF}, {"register", REGISTER}, {"return", RETURN},
+    {"short", SHORT}, {"signed", SIGNED}, {"sizeof", SIZEOF},
+    {"static", STATIC}, {"struct", STRUCT}, {"switch", SWITCH},
+    {"while", WHILE},
+};
+
+
 Buffer* allocate_buffer(int size) {
     Buffer* buffer = (Buffer*)malloc(sizeof(Buffer));
     if (buffer == NULL) {
@@ -59,11 +71,21 @@ void fill_buffer(FILE *fptr, Buffer *buffer) {
         buffer->next_char_id = 0;
     }
 }
-
+///////////////// LEXEMA //////////////////////
 int is_char(char c) {
     int ascii_code = (int)c;
-
-    if ((ascii_code >= 48 && ascii_code <= 57) || (ascii_code >= 65 && ascii_code <= 90))
+    if ((ascii_code >= 97 && ascii_code <= 122) || (ascii_code >= 65 && ascii_code <= 90))
         return 1;
     return 0;
+}
+
+
+enum Token classifyLexeme(char* lexeme) {
+    int total_words = sizeof(key_words) / sizeof(KeyWord);
+    for (int i = 0; i < total_words; i++) {
+        if (strcmp(lexeme, key_words[i].word) == 0) {
+            return key_words[i].token; // Palavra-chave encontrada
+        }
+    }
+    return ID;
 }
--- ex3/funcs.h
+++ ex3/funcs.h
@@ -1,11 +1,30 @@
 #include <stdio.h>
 
+enum Token {
+    ID, INT, CHAR, FLOAT, DOUBLE, LONG, AUTO, BREAK, CASE, CONTINUE, DEFAULT,
+    DO, ENUM, EXTERN, FOR, GOTO, IF, REGISTER, RETURN, SHORT, SIGNED,
+    SIZEOF, STATIC, STRUCT, SWITCH, WHILE
+};
+
+typedef struct {
+    const char *word;
+    enum Token token;
+} KeyWord;
+
+extern KeyWord key_words[];
+
 typedef struct {
     char* data;
     int next_char_id;
     int next_char_line;
     int size;
 } Buffer;
+
+typedef struct {
+    int line;
+    char lexema[64];
+    enum Token token;
+} Classification;
 
 Buffer* allocate_buffer(int size);
 void deallocate_buffer(Buffer* buffer);
