e99d80bad931084aca1330dfaa6e438234f0ad6b
arthur <arthursantos.oliveira001@gmail.com>
2025-01-03 20:31:51 -0300

    algoritmo de teste

--- projeto final/lexer.py
+++ projeto final/lexer.py
@@ -104,11 +104,20 @@ def analisador_lexico(codigo):
     return estado
 
 codigo = """
-int xk = 10;
-if (x <= 20) {
-    return x;
+/* Um programa para calcular o mdc
+   segundo o algoritmo de Euclides. */
+
+int gcd (int u, int v)
+{ if (v == 0) return u ;
+  else return gcd(v,u-u/v*v);
+  /* u-u/v*v == u mod v */
 }
-/* Comentário */
+
+void main(void)
+{ int x; int y;
+  x = input(); y = input();
+  output(gcd(x,y));
+}
 """
 
 if analisador_lexico(codigo) == 0:
